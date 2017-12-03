77e15b87d0829b2218bd7b87d28dfa649288ba26
Fabio A. Camichel <fabiocamichel@gmail.com>
2017-12-03 14:59:11 +0100

    feat: adding color scheme
    Adding color scheme inspired by autumn for the app

---
+++ utils/colors.js
@@ -0,0 +1,10 @@
+/* Coolors Exported Palette - coolors.co/ccc7b9-eaf9d9-e2d4ba-af7a6d-493843 */
+/* Autumn Style */
+
+export const paleSilver = 'rgba(204, 199, 185, 1)'
+export const nyanza = 'rgba(234, 249, 217, 1)'
+export const bone = 'rgba(226, 212, 186, 1)'
+export const copperPenny = 'rgba(175, 122, 109, 1)'
+export const darkPuce = 'rgba(73, 56, 67, 1)'
+export const white = '#fff'
+export const black = '#000'
