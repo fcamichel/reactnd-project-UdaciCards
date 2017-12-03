2ce962d54e6dce05d8ce2a03bf717bc01df227c9
Fabio A. Camichel <fabiocamichel@gmail.com>
2017-12-03 18:12:27 +0100

    feat: adding store and reducers to App

--- App.js
+++ App.js
@@ -1,16 +1,20 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { Provider } from 'react-redux'
+import { StyleSheet, Text, View } from 'react-native'
 import FlashStatusBar from './components/FlashStatusBar'
 import Tabs from "./components/navigation/Tabs"
 import { copperPenny, white } from './utils/colors'
+import store from "./store/index"
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={ styles.container }>
-        <FlashStatusBar backgroundColor={ copperPenny } />
-        <Tabs/>
-      </View>
+      <Provider store={ store }>
+        <View style={ styles.container }>
+          <FlashStatusBar backgroundColor={ copperPenny } />
+          <Tabs/>
+        </View>
+      </Provider>
     );
   }
 }
---
+++ utils/types.js
@@ -0,0 +1,5 @@
+export const RECEIVE_DECKS = 'RECEIVE_DECKS'
+export const ADD_DECK = 'ADD_DECK'
+export const DELETE_DECK = 'DELETE_DECK'
+export const ADD_QUESTION = 'ADD_QUESTION'
+export const ADD_DECK_DATA = 'ADD_DECK_DATA'
